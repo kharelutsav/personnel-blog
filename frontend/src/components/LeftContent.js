f441782cd38ed2097cdd3d7f3a58e1c428dfb900
kharelutsav <kharelutsav4@gmail.com>
2022-05-07 22:44:02 +0545

    Bug fixed

--- frontend/src/components/LeftContent.js
+++ frontend/src/components/LeftContent.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { CgProfile } from 'react-icons/cg'
+import { BsCardImage } from 'react-icons/bs'
 import './LeftContent.css'
 
 const Avatar = () => {
@@ -15,10 +16,14 @@ const Avatar = () => {
                     <img
                         alt=""
                         src={showImage}
-                        style={{ width: '70px', height: '70px' }}
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            borderRadius: '2.8rem',
+                        }}
                     />
                 ) : (
-                    <CgProfile style={{ width: '70px', height: '70px' }} />
+                    <CgProfile style={{ width: '100%', height: '100%' }} />
                 )}
             </label>
             <input
@@ -35,9 +40,13 @@ const Avatar = () => {
 const AuthorInfo = () => {
     return (
         <div className="author-info">
-            <label className="input-creds" htmlFor="name">Author's Name</label>
-            <input className="input-creds" name="name" id="name" /> <br/>
-            <label className="input-creds" htmlFor="phone">Phone Number</label>
+            <label className="input-creds" htmlFor="name">
+                Author's Name
+            </label>
+            <input className="input-creds" name="name" id="name" /> <br />
+            <label className="input-creds" htmlFor="phone">
+                Phone Number
+            </label>
             <input className="input-creds" name="phone" id="phone"></input>
         </div>
     )
@@ -52,12 +61,70 @@ const AboutAuthor = () => {
     )
 }
 
+const Title = () => {
+    return (
+        <div className="title-container">
+            <input
+                className="title"
+                width="100%"
+                placeholder="Please enter the title of your article."
+            />
+        </div>
+    )
+}
+
+const Thumbnail = () => {
+    const [showThumbnail, setShowThumbnail] = useState()
+    const imageSelected = (event) => {
+        const [file] = event.target.files
+        setShowThumbnail(URL.createObjectURL(file))
+    }
+    return (
+        <div className="thumbnail-container">
+            <label htmlFor="thumbnail">
+                {showThumbnail ? (
+                    <img
+                        alt=""
+                        src={showThumbnail}
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                            borderRadius: '0.5rem',
+                        }}
+                    />
+                ) : (
+                    <BsCardImage style={{ width: '100%', height: '100%' }} />
+                )}
+            </label>
+            <input
+                type="file"
+                accept="image/*"
+                style={{ display: 'none', visibility: 'none' }}
+                id="thumbnail"
+                onChange={imageSelected}
+            />
+        </div>
+    )
+}
+
+const Abstract = () => {
+    return (
+        <div className="abstract-container">
+            <textarea
+                className="abstract"
+                width="100%"
+                placeholder="Please give the abstract of your article."
+            ></textarea>
+        </div>
+    )
+}
+
 const AboutArticle = () => {
     return (
         <div className="about-article">
-            <div className='title'></div>
-            <div className='thumbnail'></div>
-            <div className='abstract'></div>
+            <Title />
+            <Thumbnail />
+            <Abstract />
         </div>
     )
 }
