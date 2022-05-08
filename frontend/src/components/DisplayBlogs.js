3b331fc0deae59d0440e7064911eeb6170f3515d
kharelutsav <kharelutsav4@gmail.com>
2022-05-08 16:36:49 +0545

    Added social links

--- frontend/src/components/DisplayBlogs.js
+++ frontend/src/components/DisplayBlogs.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './DisplayBlogs.css'
+import { SiLinkedin, SiGmail, SiGithub, SiInstagram } from 'react-icons/si'
+import { FaFacebookSquare, FaYoutube } from 'react-icons/fa'
 
 const Avatar = () => {
     return (
@@ -8,8 +10,8 @@ const Avatar = () => {
                 alt=""
                 src="#"
                 style={{
-                    width: '70px',
-                    height: '70px',
+                    width: '56px',
+                    height: '56px',
                     borderRadius: '2.8rem',
                 }}
             />
@@ -17,14 +19,73 @@ const Avatar = () => {
     )
 }
 
+const Container = ({ address, logo }) => {
+    const LOGO = logo
+    return (
+        <a href={address}>
+            <LOGO className="logos" />
+        </a>
+    )
+}
+
+const SocialLinks = () => {
+    const [Links, setLinks] = useState([
+        { address: 'https://www.linkedin.com', logo: SiLinkedin },
+        { address: 'https://www.facebook.com', logo: FaFacebookSquare },
+    ])
+    return (
+        <div className="social-container">
+            {Links.map((link, index) => {
+                return (
+                    <Container
+                        key={index}
+                        address={link.address}
+                        logo={link.logo}
+                    />
+                )
+            })}
+        </div>
+    )
+}
+
+const AboutUser = () => {
+    const [userInfo, setUserInfo] = useState({
+        name: 'Test User',
+        email: 'email@example.com',
+        phone: '0123456789',
+    })
+    return (
+        <div className="about-user">
+            <p className="user-creds user-name">{userInfo.name}</p>
+            <p className="user-creds">{userInfo.phone}</p>
+            <p className="user-creds">{userInfo.email}</p>
+        </div>
+    )
+}
+
+const Title = () => {
+    return <p>None</p>
+}
+
+const Abstract = () => {
+    return <p>None</p>
+}
+
 const ContentInfo = () => {
-    return <div className="content-disp-block"></div>
+    return (
+        <div className="content-disp-block">
+            <Title />
+            <Abstract />
+        </div>
+    )
 }
 
 const UserInfo = () => {
     return (
         <div className="user-disp-block">
+            <AboutUser />
             <Avatar />
+            <SocialLinks />
         </div>
     )
 }
