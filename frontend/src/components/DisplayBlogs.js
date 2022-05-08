93a1d9e7717d54ed837f815ac3155dd0978102f4
kharelutsav <kharelutsav4@gmail.com>
2022-05-08 15:35:39 +0545

    Responsive UI

--- frontend/src/components/DisplayBlogs.js
+++ frontend/src/components/DisplayBlogs.js
@@ -1,6 +1,43 @@
 import React from 'react'
 import './DisplayBlogs.css'
 
+const Avatar = () => {
+    return (
+        <div className="avatar">
+            <img
+                alt=""
+                src="#"
+                style={{
+                    width: '70px',
+                    height: '70px',
+                    borderRadius: '2.8rem',
+                }}
+            />
+        </div>
+    )
+}
+
+const ContentInfo = () => {
+    return <div className="content-disp-block"></div>
+}
+
+const UserInfo = () => {
+    return (
+        <div className="user-disp-block">
+            <Avatar />
+        </div>
+    )
+}
+
+const BlogInfo = () => {
+    return (
+        <div className="about-blogs">
+            <UserInfo />
+            <ContentInfo />
+        </div>
+    )
+}
+
 function DisplayBlogs() {
     return (
         <>
@@ -8,19 +45,25 @@ function DisplayBlogs() {
                 <div className="thumbnail-blogs">
                     <img src="" alt="" className="thumbnail-image" />
                 </div>
-                <div className="about-blogs">
-                    <div className="user-disp-block"></div>
-                    <div className="content-disp-block"></div>
-                </div>
+                <BlogInfo />
             </div>
             <div className="main-blogs">
                 <div className="thumbnail-blogs">
                     <img src="" alt="" className="thumbnail-image" />
                 </div>
-                <div className="about-blogs">
-                    <div className="user-disp-block"></div>
-                    <div className="content-disp-block"></div>
+                <BlogInfo />
+            </div>
+            <div className="main-blogs">
+                <div className="thumbnail-blogs">
+                    <img src="" alt="" className="thumbnail-image" />
                 </div>
+                <BlogInfo />
+            </div>
+            <div className="main-blogs">
+                <div className="thumbnail-blogs">
+                    <img src="" alt="" className="thumbnail-image" />
+                </div>
+                <BlogInfo />
             </div>
         </>
     )
