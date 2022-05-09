0b1c0dbf24b83d7eaab4e38b422ecc2cd0cd620b
kharelutsav <kharelutsav4@gmail.com>
2022-05-09 22:29:58 +0545

    Create accomplished

--- frontend/src/components/CreateNewBlog.js
+++ frontend/src/components/CreateNewBlog.js
@@ -4,12 +4,14 @@ import { BsCardImage } from 'react-icons/bs'
 import './LeftContent.css'
 import axios from './axios-config'
 
-function CreateNewBlog() {
+function CreateNewBlog({ setBlogs }) {
+    const article = {}
     const Thumbnail = () => {
         const [showThumbnail, setShowThumbnail] = useState()
         const imageSelected = (event) => {
             const [file] = event.target.files
             setShowThumbnail(URL.createObjectURL(file))
+            article.thumbnail = file.name
         }
         return (
             <div className="thumbnail-container">
@@ -42,24 +44,36 @@ function CreateNewBlog() {
     }
 
     const Title = () => {
+        const [title, setTitle] = useState('')
         return (
             <div className="title-container">
                 <input
                     className="title"
                     width="100%"
                     placeholder="Please enter the title of your article."
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                    onBlur={() => {
+                        article.title = title
+                    }}
                 />
             </div>
         )
     }
 
     const Abstract = () => {
+        const [abstract, setAbstract] = useState('')
         return (
             <div className="abstract-container">
                 <textarea
                     className="abstract"
                     width="100%"
                     placeholder="Please give the abstract of your article."
+                    value={abstract}
+                    onChange={(event) => setAbstract(event.target.value)}
+                    onBlur={() => {
+                        article.abstract = abstract
+                    }}
                 ></textarea>
             </div>
         )
@@ -75,11 +89,26 @@ function CreateNewBlog() {
         )
     }
 
+    const post_blog = () => {
+        console.log(article)
+        axios
+            .post('/create-post', {
+                article: { ...article },
+                email: 'kharelutsav4@gmail.com',
+            })
+            .then((response) => {
+                setBlogs(response.data)
+            })
+            .catch((err) => console.log(err))
+    }
+
     return (
         <div className="main-content">
             <div className="about">
                 <AboutArticle />
-                <button className="upload-btn">Post Blog</button>
+                <button className="upload-btn" onClick={() => post_blog()}>
+                    Post Blog
+                </button>
             </div>
         </div>
     )
