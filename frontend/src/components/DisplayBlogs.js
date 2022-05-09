f4b4b90b4b943a1a9cdc161040ad732f69ef7ac8
kharelutsav <kharelutsav4@gmail.com>
2022-05-09 09:51:20 +0545

    Reformatted

--- frontend/src/components/DisplayBlogs.js
+++ frontend/src/components/DisplayBlogs.js
@@ -3,91 +3,103 @@ import './DisplayBlogs.css'
 import { SiLinkedin, SiGmail, SiGithub, SiInstagram } from 'react-icons/si'
 import { FaFacebookSquare, FaYoutube } from 'react-icons/fa'
 
-const Avatar = () => {
-    return (
-        <div className="avatar-blogs">
-            <img
-                alt=""
-                src="http://localhost:3000/utsav.jpg"
-                style={{
-                    width: '56px',
-                    height: '56px',
-                    borderRadius: '2.8rem',
-                }}
-            />
-        </div>
-    )
-}
+const UserInfo = () => {
+    const AboutUser = () => {
+        const [userInfo, setUserInfo] = useState({
+            name: 'Test User',
+            email: 'email@example.com',
+            phone: '0123456789',
+        })
+        return (
+            <div className="about-user">
+                <p className="user-creds user-name">{userInfo.name}</p>
+                <p className="user-creds">{userInfo.phone}</p>
+                <p className="user-creds">{userInfo.email}</p>
+            </div>
+        )
+    }
 
-const Container = ({ address, logo }) => {
-    const LOGO = logo
-    return (
-        <a href={address}>
-            <LOGO className="logos" />
-        </a>
-    )
-}
+    const Avatar = () => {
+        return (
+            <div className="avatar-blogs">
+                <img
+                    alt=""
+                    src="http://localhost:3000/utsav.jpg"
+                    style={{
+                        width: '56px',
+                        height: '56px',
+                        borderRadius: '2.8rem',
+                    }}
+                />
+            </div>
+        )
+    }
 
-const SocialLinks = () => {
-    const [Links, setLinks] = useState([
-        { address: '#', logo: SiLinkedin },
-        { address: '#', logo: FaFacebookSquare },
-        { address: '#', logo: SiGmail },
-        { address: '#', logo: SiGithub },
-        { address: '#', logo: FaYoutube },
-        { address: '#', logo: SiInstagram },
-    ])
-    return (
-        <div className="social-container">
-            {Links.map((link, index) => {
-                return (
-                    <Container
-                        key={index}
-                        address={link.address}
-                        logo={link.logo}
-                    />
-                )
-            })}
-        </div>
-    )
-}
+    const SocialLinks = () => {
+        const [Links, setLinks] = useState([
+            { address: '#', logo: SiLinkedin },
+            { address: '#', logo: FaFacebookSquare },
+            { address: '#', logo: SiGmail },
+            { address: '#', logo: SiGithub },
+            { address: '#', logo: FaYoutube },
+            { address: '#', logo: SiInstagram },
+        ])
 
-const AboutUser = () => {
-    const [userInfo, setUserInfo] = useState({
-        name: 'Test User',
-        email: 'email@example.com',
-        phone: '0123456789',
-    })
-    return (
-        <div className="about-user">
-            <p className="user-creds user-name">{userInfo.name}</p>
-            <p className="user-creds">{userInfo.phone}</p>
-            <p className="user-creds">{userInfo.email}</p>
-        </div>
-    )
-}
-
-const Title = () => {
-    return <p className="content-title">Lorem ipsum dolor sit amet</p>
-}
-
-const Abstract = () => {
-    return (
-        <p className="content-abstract">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-            vulputate felis id magna iaculis, vel vulputate urna placerat. Sed
-            ac aliquet lacus, in convallis nibh. Praesent quam purus,
-            sollicitudin in ligula vel, molestie viverra justo. Donec
-            pulvinar....(
-            <a alt="" href="#">
-                read more
-            </a>
+        const Container = ({ address, logo }) => {
+            const LOGO = logo
+            return (
+                <a href={address}>
+                    <LOGO className="disp-logos" />
+                </a>
             )
-        </p>
+        }
+
+        return (
+            <div className="social-container">
+                {Links.map((link, index) => {
+                    return (
+                        <Container
+                            key={index}
+                            address={link.address}
+                            logo={link.logo}
+                        />
+                    )
+                })}
+            </div>
+        )
+    }
+    return (
+        <div className="user-disp-block">
+            <div className="check-mate">
+                <AboutUser />
+                <Avatar />
+            </div>
+            <SocialLinks />
+        </div>
     )
 }
 
 const ContentInfo = ({ time }) => {
+    const Abstract = () => {
+        return (
+            <p className="content-abstract">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                vulputate felis id magna iaculis, vel vulputate urna placerat.
+                Sed ac aliquet lacus, in convallis nibh. Praesent quam purus,
+                sollicitudin in ligula vel, molestie viverra justo. Donec
+                pulvinar....(
+                <a alt="" href="#">
+                    read more
+                </a>
+                )
+            </p>
+        )
+    }
+
+    const Title = () => {
+        return <p className="content-title">Lorem ipsum dolor sit amet</p>
+    }
+
     return (
         <div className="content-disp-block">
             <Title />
@@ -107,68 +119,44 @@ const ContentInfo = ({ time }) => {
     )
 }
 
-const UserInfo = () => {
-    return (
-        <div className="user-disp-block">
-            <div className="check-mate">
-                <AboutUser />
-                <Avatar />
-            </div>
-            <SocialLinks />
-        </div>
-    )
-}
-
-const BlogInfo = () => {
-    return (
-        <div className="about-blogs">
-            <UserInfo />
-            <ContentInfo />
-        </div>
-    )
-}
-
 function DisplayBlogs() {
+    const BlogInfo = () => {
+        return (
+            <div className="about-blogs">
+                <UserInfo />
+                <ContentInfo />
+            </div>
+        )
+    }
+
+    const Thumbnail = () => {
+        return (
+            <div className="thumbnail-blogs">
+                <img
+                    src="http://localhost:3000/panda.jpg"
+                    alt=""
+                    className="thumbnail-image"
+                />
+            </div>
+        )
+    }
+
     return (
         <>
             <div className="main-blogs">
-                <div className="thumbnail-blogs">
-                    <img
-                        src="http://localhost:3000/panda.jpg"
-                        alt=""
-                        className="thumbnail-image"
-                    />
-                </div>
+                <Thumbnail />
                 <BlogInfo />
             </div>
             <div className="main-blogs">
-                <div className="thumbnail-blogs">
-                    <img
-                        src="http://localhost:3000/panda.jpg"
-                        alt=""
-                        className="thumbnail-image"
-                    />
-                </div>
+                <Thumbnail />
                 <BlogInfo />
             </div>
             <div className="main-blogs">
-                <div className="thumbnail-blogs">
-                    <img
-                        src="http://localhost:3000/panda.jpg"
-                        alt=""
-                        className="thumbnail-image"
-                    />
-                </div>
+                <Thumbnail />
                 <BlogInfo />
             </div>
             <div className="main-blogs">
-                <div className="thumbnail-blogs">
-                    <img
-                        src="http://localhost:3000/panda.jpg"
-                        alt=""
-                        className="thumbnail-image"
-                    />
-                </div>
+                <Thumbnail />
                 <BlogInfo />
             </div>
         </>
